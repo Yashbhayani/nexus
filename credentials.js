256ef8c34ecaf9e21671c58bcabb2ae0c3b53bdb
Yash Bhayani <83011938+Yashbhayani@users.noreply.github.com>
2025-11-26 12:18:25 -0600

    YB -  Changes in Code

--- credentials.js
+++ credentials.js
@@ -1,17 +1,32 @@
+module.exports.HostData = {
+  Part1: "mysql-",
+  Part2: "7e0e00f-",
+  Part3: "coderecherche07-",
+  Part4: "45e2.",
+  Part5: "c.",
+  Part6: "aiven",
+  Part7: "cloud",
+  Part8: ".com",
+};
+
+module.exports.Pass = {
+  Pass1: "AV",
+  Pass2: "NS_2xRP-",
+  Pass3: "mQBoQQ14Vp3xIi",
+};
+
 module.exports.credentials = {
   DB_NAME: "nexus",
-  DB_USER: "root",
-  DB_PASSWORD: "9974143053",
-  DB_HOST: "localhost",
-  DB_PORT: 3308,
+  DB_USER: "avnadmin",
+  DB_PASSWORD: "",
+  DB_HOST: "",
+  DB_PORT: 16133,
 };
-module.exports.HostData =  {
-    Part1 : "mysql-",
-    Part2: "7e0e00f-",
-    Part3 : "coderecherche07-",
-    Part4: "45e2.",
-    Part5: "c.",
-    Part6: "aiven",
-    Part7: "cloud",
-    Part8: ".com"
-}
+
+// module.exports.credentials = {
+//   DB_NAME: "nexus",
+//   DB_USER: "root",
+//   DB_PASSWORD: "9974143053",
+//   DB_HOST: "localhost",
+//   DB_PORT: 3308,
+// };
