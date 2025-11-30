a796731f8797bd63b652ccc578b2c72157c6d5bf
sanket1001 <your-email@example.com>
2025-11-30 09:31:21 -0600

    SB - Update APIRoutes

--- src/Context/API/ApiRouter.jsx
+++ src/Context/API/ApiRouter.jsx
@@ -1,6 +1,11 @@
 //export const host = "https://nexuscampus.onrender.com/api/";
 export const host = "http://localhost:5000/api/";
 
+// Admin Routes
+export const adminpanel = "admin/adminpanel";
+export const getuserorg = "admin/getuserorg";
+export const deleteaccount = "admin/delete-account";
+
 // Auth Routes
 export const loginurl = "auth/login";
 export const signupurl = "auth/create-account";
@@ -9,30 +14,43 @@ export const verifyusertype = "auth/verifyusertype";
 export const verifyemail = "auth/verifyemail";
 export const verifyotp = "auth/verifyotp";
 export const newpassword = "auth/newpassword";
-
-// Usertype Routes
-export const usertype = "usertype/";
+export const userlogdata = "auth/user-log-data";
 
 // building Routes
 export const buildingurl = "building/";
 
+// Chat Routes
+export const chaturl = "chat/";
+export const chathealth = "chat/health";
+
 // rooms Routes
-export const getroomurl = "room?BID=";
+//export const getroomurl = "room";
 export const addroomurl = "room/";
 
 //organization Routes
 export const organizationurl = "organization/";
+export const joinorganizationurl = "organization/join";
+export const vieworganizationurl = "organization/view-organization";
 
 //events Routes
 export const eventurl = "event/";
+export const joineventurl = "event/join";
+export const deleteeventurl = "event/delete-event";
+export const approveeventurl = "event/approved";
+export const rejecteventurl = "event/rejected";
+export const admineventurl = "event/adminevent";
+
+//Homefeed Routes
+export const homeurl = "home/";
 
 //posts Routes
 export const posturl = "post/";
-export const userblogurl = "userblog/";
-export const adminblogurl = "adminblogurl/";
+export const userblogurl = "post/userblog";
+export const adminblogurl = "post/adminblogurl";
 
 //useractivity Routes
 export const academiclevel = "useractivities/academic-level/";
 export const major = "useractivities/major/";
 
-export const chaturl = "chat/";
+// Usertype Routes
+export const usertype = "usertype/";
